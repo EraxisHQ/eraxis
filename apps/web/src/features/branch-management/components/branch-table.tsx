3a2a91128b97240e25058010431caacf54fdb711
EraxisHQ <eraxis.hq@gmail.com>
2026-06-12 10:56:55 +0000

    [E040] feat: branch management completed -  Error Rectification

--- apps/web/src/features/branch-management/components/branch-table.tsx
+++ apps/web/src/features/branch-management/components/branch-table.tsx
@@ -34,40 +34,4 @@ export default function BranchTable() {
 
     </div>
   );
-}import {
-  useBranches,
-} from "../hooks/use-branches";
-
-export default function BranchTable() {
-
-  const branches =
-    useBranches();
-
-  return (
-    <div>
-
-      <h2>
-        Branches
-      </h2>
-
-      {branches.map(
-        (branch) => (
-          <div
-            key={branch.id}
-          >
-
-            <strong>
-              {branch.code}
-            </strong>
-
-            {" - "}
-
-            {branch.name}
-
-          </div>
-        )
-      )}
-
-    </div>
-  );
 }
